96bffc20b7b437ecd0ba1f2a5888317fe0aca203
FantastiCole <coleamurphy@gmail.com>
2015-06-29 20:42:00 -0400

    Have generateTweet function compose a tweet listing nail polish color, brand and collection.

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ var client = new Twitter({
 
 var params = {screen_name: 'Hey_Cole'};
 
-// client.get('statuses/user_timeline', params, function(err, tweets, res){
-// 	if (!err){
-// 		console.log(tweets[0].entities.text);
-// 	}
-// 	// if (err) throw err;
-// });
-
-// client.post('statuses/update', {status: 'fourth bot tweet!'}, function(err, tweet, res){
-// 	if (err) throw err;
-// 	console.log('Tweet: ', tweet.text); //tweet body.
-// 	// console.log('Response: ', res); //Raw response object.
-// });
-
 function getTimeline(){
 	client.get('statuses/user_timeline', params, function(err, timeline, res){
 		if (!err){
@@ -51,18 +38,23 @@ function randomizer(obj){
 function generateTweet(obj){
 	var collection = randomizer(obj);
     console.log('collection: ', collection);
-    // var inside = obj[collection];
-    // console.log('data: ', inside);
 	var color = randomizer(obj[collection]);
 	console.log('color: ', color);
 	var brand = obj[collection][color];
 	console.log('brand: ', brand);
+	var tweet = color + ' by ' + brand + ' from the ' + collection + ' Collection.'
+	if (tweet.length <= 140){
+		return tweet;
+	}
+	else{
+		generateTweet(obj);
+	}
 };
 
 // sendTweet('fifth one, oh yeah baby!');
 // getTimeline();
 // randomizer(colors[randomizer(colors)]);
-generateTweet(colors);
+console.log(generateTweet(colors));
 
 
 
